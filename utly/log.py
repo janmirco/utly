adf8adc0ade58e8a83dfa5e9d246588b399ae962
Jan Mirco Pfeifer <126580858+janmirco@users.noreply.github.com>
2025-05-23 11:25:30 +0200

    feat(log): remove start, end, cwd

--- utly/log.py
+++ utly/log.py
@@ -1,6 +1,5 @@
 import logging
 from pathlib import Path
-from typing import Callable
 
 
 def set_up(output_dir: Path, log_file_name: str = "app.log") -> None:
@@ -12,15 +11,3 @@ def set_up(output_dir: Path, log_file_name: str = "app.log") -> None:
             logging.FileHandler(output_dir / Path(log_file_name)),  # file output
         ],
     )
-
-
-def start(msg: str, log_func: Callable = logging.debug) -> None:
-    log_func(f"Starting {msg}...")
-
-
-def end(msg: str, log_func: Callable = logging.debug) -> None:
-    log_func(f"Finished {msg}.")
-
-
-def cwd(log_func: Callable = logging.debug) -> None:
-    log_func(f"Current working directory: {Path.cwd()}")
