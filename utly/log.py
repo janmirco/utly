eedc419ddc26b05ee02c59c41f30cce83ccc9ba8
Jan Mirco Pfeifer <126580858+janmirco@users.noreply.github.com>
2025-05-23 11:27:01 +0200

    docs(log/set_up): add description

--- utly/log.py
+++ utly/log.py
@@ -10,11 +10,17 @@ def set_up(
     file_output: bool = True,
     file_name: str = "app.log",
 ) -> None:
+    """
+    By default, logs to both terminal and file.
+    If both outputs are disabled, attaches a NullHandler to silence all logging output.
+    """
+
     handlers = []
     if terminal_output:
         handlers.append(logging.StreamHandler())
     if file_output:
         handlers.append(logging.FileHandler(output_dir / Path(file_name)))
+
     logging.basicConfig(
         level=level,
         format=format,
