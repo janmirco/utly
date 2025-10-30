ee59af1baaceedd61f35738ce3511bcd55ca41f7
Jan Mirco Pfeifer <126580858+janmirco@users.noreply.github.com>
2025-10-30 13:02:07 +0100

    feat(path): improve flexibility of set_up_output

--- examples/path-log-timer/main.py
+++ examples/path-log-timer/main.py
@@ -1,11 +1,15 @@
 import logging
+from pathlib import Path
 
 import utly
 
 
 def main():
     # Set up output directory and logging
-    output_dir = utly.path.set_up_output()
+    output_dir = utly.path.set_up_output(
+        output_dir_root=Path.cwd() / "output/0000-00-00",
+        no_overwrite=False,
+    )
     utly.log.set_up(output_dir)
 
     # Use timer to measure performance
--- utly/path.py
+++ utly/path.py
@@ -2,10 +2,16 @@ from datetime import datetime
 from pathlib import Path
 
 
-def set_up_output(output_root: Path = Path.cwd() / Path("output")) -> Path:
-    current_time = datetime.now().isoformat()
-    output_dir = output_root / Path(current_time)
-    output_dir.mkdir(parents=True)
+def set_up_output(
+    output_dir_root: Path = Path.cwd() / "output",
+    no_overwrite: bool = True,
+) -> Path:
+    if no_overwrite:
+        current_time = datetime.now().isoformat()
+        output_dir = output_dir_root / current_time
+    else:
+        output_dir = output_dir_root
+    output_dir.mkdir(parents=True, exist_ok=True)
     return output_dir
 
 
